49033bd0f121157488df6d950184af7c991c667b
mikaelcaetano <102097133+mikaelcaetano@users.noreply.github.com>
2022-04-03 03:52:52 -0300

    att

---
+++ Ex06/index.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carr é ${vel}km/h`)
+if (vel > 50) { 
+    console.log(`Multado`)
+}
+console.log(`Dirija com cuidado`)
---
+++ Ex07/index.js
@@ -0,0 +1,8 @@
+var cidade= 'Rio de janeiro'
+console.log(`Morando na ${cidade}`)
+if(cidade == `São Paulo`) {
+    console.log(`vc é paulista`)
+}
+else{
+    console.log(`vc é estrangeiro`)
+}
