e529710e1eba8a23c1ad295351dd80bcfdc62216
mikaelcaetano <102097133+mikaelcaetano@users.noreply.github.com>
2022-04-03 12:06:04 -0300

    att

---
+++ Ex09/index.js
@@ -0,0 +1,11 @@
+var idade = 12
+console.log(`Vc tem ${idade} anos`)
+if(idade <16) {
+    console.log('Não vota')
+}
+else if(idade < 18 || idade > 65){
+    console.log('Voto opcional')
+}
+else{
+    console.log('Voto obrigatorio')
+}
---
+++ Ex10/index.js
@@ -0,0 +1,11 @@
+var hora=1
+console.log(`Agora são exatamente ${hora} horas`)
+if(hora <12){
+    console.log('bom dia!!!')
+}
+else if(hora <=18){
+    console.log('Boa tarde!!!')
+}
+else{
+    console.log('Boa noite!!!')
+}
