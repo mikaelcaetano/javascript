f9994a431bdeff6bbae5f191456f19af1afb3f9c
mikaelcaetano <102097133+mikaelcaetano@users.noreply.github.com>
2022-04-03 21:12:34 -0300

    att

--- Ex12/script.js
+++ Ex12/script.js
@@ -1,19 +1,19 @@
 function carregar(){
-    var msg=window.document.getElementById('msg')
-    var img=window.document.getElementById('imagem')
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
     var data = new Date()
-    var hora = 20 //data.getHours()
-    msg.innerHTML=`agr são ${hora} horas`
-    if(hora >= 0 && hora <12){
-        img.src='manha.jpeg'
+    var hora = data.getHours()
+    msg.innerHTML = `agr são ${hora} horas`
+    if(hora >= 0 && hora < 12) {
+        img.src='ex/manha.jpeg'
         document.body.style.background='yellow'
     }
-    else if(hora>=12&&hora<=18){
-        img.src='tarde.jpeg'
+    else if(hora >= 12 && hora < 18) {
+        img.src='ex/tarde.jpeg'
         document.body.style.background='red'
     }
-    else{
-        img.src='noite.jpeg'
+    else {
+        img.src='ex/noite.jpeg'
         document.body.style.background='grey'
     }
 }
